6ecb8734f234684a8c2189fe51048ae73c7f3a0a
Anton Revta <anton.revta@gmail.com>
2023-05-07 23:24:57 +0300

    move sections in separate classes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,99 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: const Text(
+                      'Oeschinen Lake Campground',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Kandersteg, Switzerland',
+                    style: TextStyle(
+                        color: Colors.grey[500]
+                    ),
+                  )
+                ],
+              )
+          ),
+          const FavoriteWidget()
+        ],
+      ),
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+        padding: EdgeInsets.all(32),
+        child: Text(
+            'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+                'Alps. Situated 1,578 meters above sea level, it is one of the '
+                'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+                'half-hour walk through pastures and pine forest, leads you to the '
+                'lake, which warms to 20 degrees Celsius in the summer. Activities '
+                'enjoyed here include rowing, and riding the summer toboggan run.',
+            softWrap: true
+        )
+    );
+  }
+}
+
+class ButtonsSection extends StatelessWidget {
+  const ButtonsSection({super.key});
+  Color _color(BuildContext context) {
+    return Theme.of(context).primaryColor;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(_color(context), 'CALL', Icons.call),
+        _buildButtonColumn(_color(context), 'ROUTE', Icons.near_me),
+        _buildButtonColumn(_color(context), 'SHARE', Icons.share),
+      ],
+    );
+  }
+
+  Column _buildButtonColumn(Color color, String label, IconData icon) {
+    return Column(
+      children: [
+        Icon(
+            icon,
+            color: color
+        ),
+        Text(
+          label,
+          style: TextStyle(
+              color: color
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class FavoriteWidget extends StatefulWidget {
   const FavoriteWidget({super.key});
 
@@ -70,88 +163,13 @@ class MyApp extends StatelessWidget {
               height: 240,
               fit: BoxFit.cover,
             ),
-            _titleSection(context),
-            _buttonsSection(context),
-            _textSection(context)
+            const TitleSection(),
+            const ButtonsSection(),
+            const TextSection()
           ],
         )
       ),
     );
   }
-
-  Column _buildButtonColumn(Color color, String label, IconData icon) {
-    return Column(
-      children: [
-        Icon(
-          icon,
-          color: color
-        ),
-        Text(
-          label,
-          style: TextStyle(
-            color: color
-          ),
-        )
-      ],
-    );
-  }
-  Color _color(BuildContext context) {
-    return Theme.of(context).primaryColor;
-  }
-  Widget _buttonsSection(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(_color(context), 'CALL', Icons.call),
-        _buildButtonColumn(_color(context), 'ROUTE', Icons.near_me),
-        _buildButtonColumn(_color(context), 'SHARE', Icons.share),
-      ],
-    );
-  }
-  Widget _titleSection(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: const Text(
-                      'Oeschinen Lake Campground',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Kandersteg, Switzerland',
-                    style: TextStyle(
-                      color: Colors.grey[500]
-                    ),
-                  )
-                ],
-              )
-          ),
-          const FavoriteWidget()
-        ],
-      ),
-    );
-  }
-  Widget _textSection(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-              'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-              'Alps. Situated 1,578 meters above sea level, it is one of the '
-              'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-              'half-hour walk through pastures and pine forest, leads you to the '
-              'lake, which warms to 20 degrees Celsius in the summer. Activities '
-              'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true
-      )
-    );
-  }
 }
+
